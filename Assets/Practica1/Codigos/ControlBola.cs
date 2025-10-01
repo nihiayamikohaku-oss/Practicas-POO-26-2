2e2167484d52684ebb5cfc92ab60eb1c60123cd0
nihiayamikohaku-oss <nihiayamikohaku@gmail.com>
2025-10-01 10:50:46 -0600

    actualización 01/10/25
    - Se agrego el código de apuntar la bola de boliche
    - Se se agrego código para cuando sea lanzada la bola la cámara la siga
    -se agrego modelo 3D con texturas y animación

--- Assets/Practica1/Codigos/ControlBola.cs
+++ Assets/Practica1/Codigos/ControlBola.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class ControlBola : MonoBehaviour
 {
+    public Transform CamaraPrincipal;
+
     public Rigidbody rb;
+
+    public float velocidadDeApuntado = 5f;
+    public float limiteDerecho = 2;
+    public float limiteIzquierdo = -2;
     public float fuerzaDeLanzamiento = 10000f;
+
+    private bool haSidoLanzada = false;
  
     // Start is called before the first frame update
     void Start()
@@ -15,11 +23,44 @@ public class ControlBola : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+    {//Expresion: mientras haSidoLanzada
+        if (haSidoLanzada == false)
         {
-            rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
-            Debug.Log("Yo sacado de onda");
+
+            Apuntar();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Lanzar();
+            }
         }
     }
-}
+
+    void Apuntar()
+    {
+        /*1. Leer un input horizontal de tipo axis te permite registrar
+         entradas con las teclas A y D, y flecha izquierda y flecha derecha*/
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        /*2.mover la bola hacia los lados*/
+        transform.Translate(Vector3.right * inputHorizontal * velocidadDeApuntado * Time.deltaTime);
+
+        /*3. Delimitar el movimiento de la bola*/
+        Vector3 posicionActual = transform.position;
+
+        posicionActual.x = Mathf.Clamp(posicionActual.x, limiteIzquierdo, limiteDerecho);
+
+        transform.position = posicionActual;
+    }
+   
+    void Lanzar()
+    {
+        haSidoLanzada = true;
+        rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
+
+        if (CamaraPrincipal != null)
+        {
+            CamaraPrincipal.SetParent(transform);
+        }
+    }
+
+}//BIENVENIDO A LA ENTRADA AL INFIERNO
